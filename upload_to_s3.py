10b3544b4cb53808a6bb35d720f199010a1cc800
TROIS <franciscangeno1@gmail.com>
2024-05-07 11:25:40 +0300

    Modify upload to s3

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -3,10 +3,11 @@ import boto3
 s3 = boto3.client("s3")
 target_bucket = "customer-debit-card-daily-transactions"
 
-def upload_to_s3(filename, date_str):
+def upload_to_s3(filename, target_bucket, date_str):
     """Uploads a CSV file to S3 with Hive-style partitioning"""
     file_path = f"raw_data/date={date_str}"  # Hive-style partitioning
     s3.upload_file(f'/{filename}', target_bucket, file_path)
     print(f"File uploaded to S3 Bucket {target_bucket}/{file_path}")
     return
+
 
