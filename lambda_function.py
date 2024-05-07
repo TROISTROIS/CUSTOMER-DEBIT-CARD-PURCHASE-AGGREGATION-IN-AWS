4b0ca3683db61a625135bdacab56f0a26a4d9afd
TROIS <franciscangeno1@gmail.com>
2024-05-07 11:48:51 +0300

    Update Lambda Function

--- lambda_function.py
+++ lambda_function.py
@@ -2,33 +2,24 @@ import json
 from mock_data_generator import generate_data
 from upload_to_s3 import upload_to_s3
 from rds_connect import connect_and_create_db
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 
-# Set the current date as today
-start_date = datetime.now().date()
-num_days = 5
+# Define date range (modify as needed)
+start_date = date(2024, 5, 2)  # Adjust start date
+end_date = date.today()
 
 
 def lambda_handler(event, context):
-    current_date = start_date
-
-    # Generate data for 5 days
-    for i in range(num_days):
-        # Convert the current date to a string
-        date_str = current_date.strftime("%Y-%m-%d")
-
-        # Call the generate_data function with the current date and date string
+    for current_date in range((end_date - start_date).days + 1):
+        # Generate Date
+        current_date = start_date + timedelta(days=current_date)
+        date_str = str(current_date)
         generate_data(current_date, date_str)
-
         # Upload the generated CSV to S3
         upload_to_s3(f"transactions_{date_str}.csv", date_str)
 
-        # Increment the current date by one day
-        current_date += timedelta(days=1)
-
     # Connect to RDS, create customers database and customer_transactions table
     connect_and_create_db()
-
     return {
         'statusCode': 200,
         'body': json.dumps(f'Customer Data Generated, Created new RDS DB and Table.')
